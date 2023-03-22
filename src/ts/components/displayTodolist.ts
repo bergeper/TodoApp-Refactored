c1de44527b3907b163b8b084644eb7e8d6597600
Per Berge <per.berge93@gmail.com>
2023-03-22 21:17:37 +0100

    (refactor): datatypes for elements.

--- src/ts/components/displayTodolist.ts
+++ src/ts/components/displayTodolist.ts
@@ -1,4 +1,5 @@
 import type { Todo } from '../models/todo';
+import { checkTodoDone } from './checkTodo';
 import { removeTodo } from './removeTodo';
 
 const todoListContainer = document.getElementById('todoListDisplay') as HTMLDivElement;
@@ -6,30 +7,22 @@ const todoListContainer = document.getElementById('todoListDisplay') as HTMLDivE
 export function createTodoList(todoList: Todo[]): void {
   todoListContainer.innerHTML = '';
   for (let i = 0; i < todoList.length; i++) {
-    const listItemContainer = document.createElement('div');
+    const listItemContainer: HTMLDivElement = document.createElement('div');
     listItemContainer.classList.add('card__task');
 
-    const listItemTask = document.createElement('p');
+    const listItemTask: HTMLParagraphElement = document.createElement('p');
     listItemTask.classList.add('card__task--name');
 
-    const listItemRemove = document.createElement('span');
+    const listItemRemove: HTMLSpanElement = document.createElement('span');
     listItemRemove.classList.add('card__task--remove');
 
-    const listItemChecked = document.createElement('input');
+    const listItemChecked: HTMLInputElement = document.createElement('input');
     listItemChecked.classList.add('card__task--check');
     listItemChecked.type = 'checkbox';
     listItemChecked.checked = todoList[i].completed;
 
     listItemChecked.addEventListener('change', () => {
-      if (listItemChecked.checked) {
-        todoList[i].completed = true;
-        listItemContainer.classList.add('card__task--done');
-        console.log(todoList);
-      } else {
-        todoList[i].completed = false;
-        listItemContainer.classList.remove('class', 'card__task--done');
-      }
-      localStorage.setItem('Todos', JSON.stringify(todoList));
+      checkTodoDone(listItemContainer, listItemChecked, todoList, i);
     });
 
     if (listItemChecked.checked) {
