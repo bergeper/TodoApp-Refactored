f609ddc8b2f6c6b762fd625d49c3c8d92234be30
Per Berge <per.berge93@gmail.com>
2023-03-22 11:53:29 +0100

    (refactor): ESLint errors resolved

--- src/ts/helpers/localStorage.ts
+++ src/ts/helpers/localStorage.ts
@@ -4,7 +4,7 @@ let todosFromLS: Todo[] = [];
 
 export function getTodosFromLS(): Todo[] {
   todosFromLS = JSON.parse(localStorage.getItem('Todos') || '[]');
-  let todos: Todo[] = todosFromLS.map(todo => {
+  const todos: Todo[] = todosFromLS.map(todo => {
     return new Todo(todo.task, todo.completed);
   });
   return todos;
