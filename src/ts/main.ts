600ad5bd743caefb52e123e45a2be77bbd8bf3cd
Per Berge <per.berge93@gmail.com>
2023-03-22 21:58:24 +0100

    (refactor): sortTodos from main.ts
    and added fn to run app.

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,20 +3,12 @@ import { createTodoInput } from './components/createTodo';
 import { createTodoList } from './components/displayTodolist';
 import { getTodosFromLS } from './helpers/localStorage';
 import type { Todo } from './models/todo';
-
 let todoList: Todo[] = [];
 todoList = getTodosFromLS();
 
-// sort button
-const buttonSort = document.getElementById('sortTask') as HTMLButtonElement;
-buttonSort.addEventListener('click', sortTodos);
-
-// sorts based on a boolean in the property completed.
-function sortTodos(): void {
-  todoList.sort((a, b) => (a.completed > b.completed ? 1 : -1));
+function runApp(): void {
+  createTodoInput();
   createTodoList(todoList);
 }
 
-// Adding objects to the list
-createTodoInput();
-createTodoList(todoList);
+runApp();
