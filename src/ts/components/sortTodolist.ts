dcf8723cbc744e9744c532262e3568ee06dbeee5
Per Berge <per.berge93@gmail.com>
2023-03-23 10:25:07 +0100

    (refactor): variablenames more conventional

--- src/ts/components/sortTodolist.ts
+++ src/ts/components/sortTodolist.ts
@@ -1,7 +1,7 @@
 import type { Todo } from '../models/todo';
 import { createTodoList } from './displayTodolist';
 
-export function sortTodos(todoList: Todo[]): void {
-  todoList.sort((a, b) => (a.completed > b.completed ? 1 : -1));
-  createTodoList(todoList);
+export function sortTodos(todos: Todo[]): void {
+  todos.sort((a, b) => (a.completed > b.completed ? 1 : -1));
+  createTodoList(todos);
 }
