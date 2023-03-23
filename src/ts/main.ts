a82a9bab198d9405c266a8dc4424010d87dfbb76
Per Berge <per.berge93@gmail.com>
2023-03-23 09:40:43 +0100

    (refactor): variable name more conventional

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,12 +3,13 @@ import { createTodoInput } from './components/createTodo';
 import { createTodoList } from './components/displayTodolist';
 import { getTodosFromLS } from './helpers/localStorage';
 import type { Todo } from './models/todo';
-let todoList: Todo[] = [];
-todoList = getTodosFromLS();
+
+let todos: Todo[] = [];
+todos = getTodosFromLS();
 
 function runApp(): void {
   createTodoInput();
-  createTodoList(todoList);
+  createTodoList(todos);
 }
 
 runApp();
